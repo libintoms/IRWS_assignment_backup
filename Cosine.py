40c65e50071c30712543937b26ff585d5a535fc7
libin <libinthomas.na@student.griffith.ie>
2021-12-13 21:57:05 +0000

    Checking if readme file is updated

--- Cosine.py
+++ Cosine.py
@@ -40,59 +40,30 @@ for filename in os.listdir(input_folder_path):
         doc2_freq.append(items_array[2])
         doc3_freq.append(items_array[3])
 
-#Converting string to int
+#Converting string to float for frequencies
 doc1_freq=[float(a) for a in doc1_freq]
 doc2_freq=[float(a) for a in doc2_freq]
 doc3_freq=[float(a) for a in doc3_freq]
 
-print("Frequencies in document 1:")
-print(doc1_freq)
-print("*"*20)
-print("Frequencies in document 2:")
-print(doc2_freq)
-print("*"*20)
-print("Frequencies in document 3:")
-print(doc3_freq)
-print("*"*20)
+#Cosine similarity between the documents
+#cosine simlarity between doc1 and doc2
+CoSim_d1_d2="The cosine similarity between doc1 and doc2: "+str(Cos_Sim(doc1_freq,doc2_freq))
+print(CoSim_d1_d2)
 
-squared_freq_doc1=[n**2 for n in doc1_freq]
-print(squared_freq_doc1)
-squared_freq_doc2=[n**2 for n in doc2_freq]
-print(squared_freq_doc2)
-squared_freq_doc3=[n**2 for n in doc3_freq]
-print(squared_freq_doc3)
+#cosine simlarity between doc2 and doc3
+CoSim_d2_d3="The cosine similarity between doc2 and doc3: "+str(Cos_Sim(doc2_freq,doc3_freq))
+print(CoSim_d2_d3)
 
-sum_freq_doc1=sum(squared_freq_doc1)
-print(sum_freq_doc1)
-sum_freq_doc2=sum(squared_freq_doc2)
-print(sum_freq_doc2)
-sum_freq_doc3=sum(squared_freq_doc3)
-print(sum_freq_doc3)
+#cosine simlarity between doc1 and doc3
+CoSim_d1_d3="The cosine similarity between doc2 and doc3: "+str(Cos_Sim(doc1_freq,doc3_freq))
+print(CoSim_d1_d3)
 
-sqrt_doc1=math.sqrt(sum_freq_doc1)
-print(sqrt_doc1)
-sqrt_doc2=math.sqrt(sum_freq_doc2)
-print(sqrt_doc2)
-sqrt_doc3=math.sqrt(sum_freq_doc3)
-print(sqrt_doc3)
+#Path to output
+f=open(os.path.join(output_folder_path,'Cosine_similarity_results.txt'),'w+')
 
-CrossProd_d1_d2=sqrt_doc1*sqrt_doc2
-CrossProd_d2_d3=sqrt_doc2*sqrt_doc3
-CrossProd_d1_d3=sqrt_doc1*sqrt_doc3
-print("The denominator for d1 & d2:"+str(CrossProd_d1_d2))
-print("The denominator for d2 & d3:"+str(CrossProd_d2_d3))
-print("The denominator for d1 & d3:"+str(CrossProd_d1_d3))
-
-#Dot product code
-DotProd_d1_d2= sum(n1*n2 for n1, n2 in zip(doc1_freq, doc1_freq))
-print(DotProd_d1_d2)
-DotProd_d2_d3= sum(n1*n2 for n1, n2 in zip(doc2_freq, doc3_freq))
-print(DotProd_d2_d3)
-DotProd_d1_d3= sum(n1*n2 for n1, n2 in zip(doc1_freq, doc3_freq))
-print(DotProd_d1_d3)
-
-#final output
-
+# #file write
+f.write("%s\n%s\n%s\n" % (CoSim_d1_d2, CoSim_d2_d3, CoSim_d1_d3))  
+f.close()
 
 
 
